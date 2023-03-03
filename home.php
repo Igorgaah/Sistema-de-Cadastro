8d46eff22dba8346ca138619a40b44d8dadd11b3
Igor Rafael Marcelo Morais <94909370+Igorgaah@users.noreply.github.com>
2023-03-03 12:48:29 -0300

    Initial commit

---
+++ home.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SITE | PMI</title>
+    <style>
+        body{
+            font-family: Arial, Helvetica, sans-serif;
+            background: #A3A3A3 none no-repeat scroll 0px 0px;
+            text-align: center;
+            color: white;
+        }
+        .box{
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%,-50%);
+            background-color: rgba(0, 0, 0, 0.6);
+            padding: 30px;
+            border-radius: 10px;
+        }
+        a{
+            text-decoration: none;
+            color: white;
+            border: 3px solid dodgerblue;
+            border-radius: 10px;
+            padding: 10px;
+        }
+        a:hover{
+            background-color: dodgerblue;
+        }
+    </style>
+</head>
+<body>
+    <div class="box">
+    <img src="img/Logo-1.png" alt="Logo prefeitura">
+    <br><br>
+        <a href="login.php">Login</a>
+        <div class="gender-title">
+            <h6>Ainda não tem uma conta? <a href="formulario.php">Crie uma</a></h6>
+        </div>
+    </div>
+</body>
+</html>
